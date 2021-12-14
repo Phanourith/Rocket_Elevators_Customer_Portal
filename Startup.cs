5ff0af55df22e9478e001f6911d906193f44991b
longviddd <73258079+longviddd@users.noreply.github.com>
2021-12-14 13:56:36 -0500

    fixed nav bar

--- Startup.cs
+++ Startup.cs
@@ -41,6 +41,7 @@ namespace RocketElevatorsCustomerPortal
             services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
